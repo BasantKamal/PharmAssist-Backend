a4376944eba22675007d3ed675dd2fe6e1493d33
Basant Kamal <basantkamalsalah@gmail.com>
2025-06-11 17:46:16 +0300

    Latest basket controller

--- PharmAssist/obj/Release/net8.0/PharmAssist.AssemblyInfo.cs
+++ PharmAssist/obj/Release/net8.0/PharmAssist.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PharmAssist")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+562ca78620e3a7419f1cace2bb136e54d1846e77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac7e07be836645f56dc9e4a0276dc46e3a4bcec")]
 [assembly: System.Reflection.AssemblyProductAttribute("PharmAssist")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PharmAssist")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
